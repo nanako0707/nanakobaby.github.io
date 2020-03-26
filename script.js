350f9e61adb7b293211a06b228031027b30761bc
nanakobaby <61221142+nanakobaby@users.noreply.github.com>
2020-03-26 15:41:44 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+$(function() {
+  $('#language-wrapper1').hover(
+    function() {
+      $('.language-text1').fadeIn();
+    },
+    function() {
+      $('.language-text1').fadeOut();
+    }
+  );
+});
+
+$(function() {
+  $('#language-wrapper2').hover(
+    function() {
+      $('.language-text2').fadeIn();
+    },
+    function() {
+      $('.language-text2').fadeOut();
+    }
+  );
+});
+
+$(function() {
+  $('#language-wrapper3').hover(
+    function() {
+      $('.language-text3').fadeIn();
+    },
+    function() {
+      $('.language-text3').fadeOut();
+    }
+  );
+});
+
+
