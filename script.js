b9ac86902723f2767e6eb9984850e43577d82efb
nanakobaby <61221142+nanakobaby@users.noreply.github.com>
2020-03-28 19:03:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-$(function() {
-  $('#language-wrapper1').hover(
-    function() {
-      $('.language-text1').fadeIn();
-    },
-    function() {
-      $('.language-text1').fadeOut();
-    }
-  );
+$(document).ready(function(){
+  $('.more').on( 'click' ,function(){
+
+  $panel = $(this).parent();
+  $panelChild = $panel.children('.hideContent');
+
+  if( !$panelChild.hasClass('open') ) {
+    $panelChild.slideDown().addClass('open');
+    $panel.children('.more').text('閉じる');
+  } else {
+    $panelChild.slideUp().removeClass('open');
+    $panel.children('.more').text('続きを見る');
+  }
+  return false;
+  });
 });
 
-$(function() {
-  $('#language-wrapper2').hover(
-    function() {
-      $('.language-text2').fadeIn();
-    },
-    function() {
-      $('.language-text2').fadeOut();
-    }
-  );
-});
-
-$(function() {
-  $('#language-wrapper3').hover(
-    function() {
-      $('.language-text3').fadeIn();
-    },
-    function() {
-      $('.language-text3').fadeOut();
-    }
-  );
-});
